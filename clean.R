c7a0f34ba5a1c3e285ea072f9acad5a677497ccd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:20:24 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,18 +1,20 @@
 
-clean_data <- raw_data
-readStates <- function(states)
-{
-    states <- states[-1,]
-    
-    num.row<- nrow(states)
-    states<- states[-num.row,]
-    
-    states <- states[,-(1:4)]
-    
-    colnames(states) <- c("stateName", "population", "popOver18", "percentOver18")
 
-    return (states)
+#creating a function to clean the dataset
+readStates<-function(states){
+#removing first row
+states<-states[-1,]
+#counting number of rows
+num.row<-nrow(states)
+#removing last row
+states<-states[-num.row,]
+#removing columns 1 to 4
+states<-states[,-1:-4]
+#changing column names
+colnames(states)<-c("stateName","population","popOver18","percentOver18")
+return(states)
 }
-
-cleanCensus <- readStates(raw_data)
-str(cleanCensus)
+#retured clean dataset 
+cleanCensus<-readStates(raw_data)
+#structure of cleaned dataset
+str(cleanCensus) 
